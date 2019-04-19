9dca0c89c6721e399e173c331d486a0b35d1cfb0
Tim Hannifan <hannifan@gmail.com>
2019-04-18 23:59:49 -0500

    cleanup

--- kit.py
+++ kit.py
@@ -5,16 +5,16 @@ import reader
 import dataparser
 import features
 import classifier
+import evaluate
 # import explorer
-
-
 
 tools = {
     'pipeline': pipeline.Pipeline(),
     'csvreader': reader.CSVReader(),
     'dataparser': dataparser.DataParser(),
     'features': features.FeatureGenerator(),
-    'classifier': classifier.Classifier()
+    'classifier': classifier.Classifier(),
+    'evaluate': evaluate.ModelEvaluator()
 }
 
 def small_demo():
@@ -26,9 +26,10 @@ def small_demo():
     parse_step = tools['dataparser']
     features_step = tools['features']
     classify_step = tools['classifier']
+    evaluate_step = tools['evaluate']
     pipe.clear()
     
-    read_step.load('data/credit-data-small.csv')
+    read_step.load('data/credit-data.csv')
     pipe.add(read_step)
 
     parse_step.configure({
@@ -38,15 +39,21 @@ def small_demo():
 
     features_step.configure({
         'discretize': [('MonthlyIncome', ['low', 'med', 'high'])],
-        'dummify': ['discrete_MonthlyIncome']
+        'dummify': ['discrete_MonthlyIncome', 'SeriousDlqin2yrs']
     })
     pipe.add(features_step)
 
     classify_step.configure({
         'type': 'DecisionTreeClassifier',
-        'target': 'SeriousDlqin2yrs'
+        'target': 'SeriousDlqin2yrs_1'
     })
     pipe.add(classify_step)
+
+    evaluate_step.configure({
+        'metrics': ['accuracy_score']
+    })
+    print('just configured')
+    pipe.add(evaluate_step)
 
     result = pipe.execute()
     print('pipe completed', type(result))
--- lib/classifier.py
+++ lib/classifier.py
@@ -34,18 +34,30 @@ class Classifier:
 
         self.config = params
 
+    def generate_yhats(self, x_data):
+        self.y_hats = self.trained_model.predict(x_data)
+
     def execute(self):
         print('classifier execution called')
         df = self.input
+        # print(df[''].value_counts())
         target = self.config['target']
+
+        # print(target)
         classifier_type = self.config['type']
 
         self.y_train = df.loc[:, [target]]
 
+        print(self.input)
+        # print(df.columns)
         s = set(df.columns)
+
         s.remove(target)
         self.x_train = df.loc[:, list(s)]
 
         self.trained_model = self.model.fit(self.x_train, self.y_train)
+        ## using same data for train/test
+        self.generate_yhats(self.x_train)
 
-        return self.trained_model
+        return self
+        
